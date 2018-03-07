fccd1e609d1b617dc84ddc52973174deef06a693
miza <mizzzamidin@gmail.com>
2018-03-08 03:25:26 +0600

    #new admin zhane users bet HTML

---
+++ AppData/Local/Programs/Python/Python36-32/S.U/Scripts/satbayev_university/catalog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Student, Teacher, StudentInformation, Spec
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_students=Student.objects.all().count()
+    num_informations=StudentInformation.objects.all().count()
+    # Available books (status = 'a')
+    num_informations_available=StudentInformation.objects.filter(status__exact='a').count()
+    num_teachers=Teacher.objects.count()  # The 'all()' is implied by default.
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_students':num_students,'num_informations':num_informations,'num_informations_available':num_informations_available,'num_teachers':num_teachers},
+    )
