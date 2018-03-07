86701b77cff65bfbebab47d7f1519246293e985b
miza <mizzzamidin@gmail.com>
2018-03-07 19:24:28 +0600

    #1 Admin бет

---
+++ AppData/Local/Programs/Python/Python36-32/S_U/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\AppData\Local\Programs\Python\Python36-32\S_U\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
